c0a26688f558b3a5f24ca124f50e1860447eaeb5
Mason Storey <storeyme2000@gmail.com>
2024-01-21 12:24:54 +0000

    Removing details

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-REM C:/Users/g7enn/AppData/Local/Programs/Python/Python311/python.exe c:/Users/g7enn/OneDrive/Documents/rsInputs/parseRsData.py
 echo Launching python script...
 python %0\..\rsSearch.py
 echo Python script finished!
